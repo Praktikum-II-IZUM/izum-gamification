706d215dbe5759dba8d4dc0be6bed85019c92614
MikeDQG <mike.govedic@gmail.com>
2025-06-08 21:27:09 +0200

    BUG FIX: vercel - unused components

--- puzzle-game/src/components/BookInfo.tsx
+++ puzzle-game/src/components/BookInfo.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Button } from "@/components/ui/button";
 import { BookCover } from '@/types/book';
-import { ExternalLink } from 'lucide-react';
 
 interface BookInfoProps {
   book: BookCover | null;

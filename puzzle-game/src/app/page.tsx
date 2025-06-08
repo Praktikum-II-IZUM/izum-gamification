5b179fb12a137be628388b9673a784029fbde450
alesfc1 <box.ales@gmail.com>
2025-06-08 20:32:46 +0200

    polepšanje gumba sign out

--- puzzle-game/src/app/page.tsx
+++ puzzle-game/src/app/page.tsx
@@ -173,7 +173,12 @@ const Page = () => {
           </button>
         </div> */}
         <div style={{color: 'red'}}>
-          <button onClick={handleSignOut}>Sign Out</button>
+        <button 
+  onClick={handleSignOut}
+  className="absolute top-4 right-4 px-4 py-2 bg-red-600 hover:bg-red-700 text-white font-medium rounded-lg transition-colors duration-200 shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+>
+  Sign Out
+</button>
         </div>
         <AppHeader />
         {!gameStarted ? (
